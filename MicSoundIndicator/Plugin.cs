116f52bc98cbe3a6806aef2675397c90e1b5eb94
xKiraiChan <xKiraiChan@gmail.com>
2022-08-03 16:01:47 -0400

    new mod: MicSoundIndicator

---
+++ MicSoundIndicator/Plugin.cs
@@ -0,0 +1,61 @@
+﻿using ABI_RC.Core;
+using ABI_RC.Core.Base;
+using BepInEx;
+using HarmonyLib;
+using System;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+
+namespace com.github.xKiraiChan.CVRPlugins.MicSoundIndicator
+{
+    [BepInPlugin(GUID, "MicSoundIndicator", "0.1.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string GUID = "com.github.xKiraiChan.CVRPlugins.MicSoundIndicator";
+
+        private static readonly Assembly assembly = Assembly.GetExecutingAssembly();
+        private static AudioSource mute;
+        private static AudioSource unmute;
+
+        private void Awake()
+        {
+            GameObject go = new("MicSoundIndicator");
+            DontDestroyOnLoad(go);
+
+            (mute = go.AddComponent<AudioSource>()).clip = LoadAudioClip("Mute.dat");
+            (unmute = go.AddComponent<AudioSource>()).clip = LoadAudioClip("Unmute.dat");
+
+            new Harmony(GUID).Patch(
+                typeof(Audio).GetMethod(nameof(Audio.SetMicrophoneActive)),
+                new(typeof(Plugin).GetMethod(nameof(Plugin.HookSetMicrophoneActive), BindingFlags.NonPublic | BindingFlags.Static))
+            );
+        }
+
+        private static AudioClip LoadAudioClip(string fileName)
+        {
+            MemoryStream mem = new();
+            assembly.GetManifestResourceStream(GUID + ".Resources." + fileName).CopyTo(mem);
+            byte[] bytes = mem.ToArray();
+            mem.Dispose();
+
+            int samples = bytes.Length / 4;
+            float[] data = new float[samples];
+            Buffer.BlockCopy(bytes, 0, data, 0, bytes.Length);
+
+            AudioClip clip = AudioClip.Create(fileName, samples, 2, 44100, false);
+            clip.SetData(data, 0);
+            return clip;
+        }
+
+        private static bool HookSetMicrophoneActive(bool __0)
+        {
+            if (__0 == RootLogic.Instance.comms.IsMuted)
+                return false;
+
+            (__0 ? mute : unmute).Play();
+
+            return true;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -6,8 +6,11 @@ Freeze your position when you open the menus.
 
 ## [LocalMirror](https://github.com/xKiraiChan/CVRPlugins/releases/latest/download/LocalMirror.dll)
 - Press `L` to create a mirror in front of you.
-- Only you can see this mirror
-- Edit the config file to change the keybind and scaling
+- Only you can see this mirror.
+- Edit the config file to change the keybind and scaling.
+
+## [MicSoundIndicator](https://github.com/xKiraiChan/CVRPlugins/releases/latest/download/MicSoundIndicator.dll)
+Plays a sound when the microphone is muted or unmuted.
 
 # CVR ToS §7.5
 When releasing mods to the public, it is required to state, that the mod authors and modification are in no affiliation with ABI and not supported by ABI. :trollface:
